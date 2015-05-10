a33c1964320d0b7631a5d90f0346fd3c1e56b7b5
Ryan Bartsch <yesmarketau@gmail.com>
2015-05-10 12:45:51 +0930

    added comment

--- SpecFlowHelloWorld.Tests/LVRCalculationSteps.cs
+++ SpecFlowHelloWorld.Tests/LVRCalculationSteps.cs
@@ -24,6 +24,7 @@ namespace SpecFlowHelloWorld.Tests
         [Given(@"a property price of (.*)")]
         public void GivenAPropertyPriceOf(Decimal propertyPrice)
         {
+            //test
             _propertyPrice = propertyPrice;
         }
 
